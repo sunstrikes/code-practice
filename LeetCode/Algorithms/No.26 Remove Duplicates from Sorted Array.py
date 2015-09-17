2f91232be02457572f4ac4d7fc4e092261f384a3
Mickey Sun <sunstrikes@163.com>
2015-09-18 00:34:48 +0800

    list element erase

---
+++ LeetCode/Algorithms/No.26 Remove Duplicates from Sorted Array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        x = -200000000
+        ret = 0
+        for i in nums:
+            if i != x:
+                ret += 1
+                x = i
+        return ret
+     
