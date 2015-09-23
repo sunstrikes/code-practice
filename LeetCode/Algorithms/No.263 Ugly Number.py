8469f162e880d77fa861720045e4d1cc9a3f0c5f
Mickey Sun <sunstrikes@163.com>
2015-09-23 11:50:12 +0800

    find 4，print False!

---
+++ LeetCode/Algorithms/No.263 Ugly Number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return False
+        if num>0 and num <6:
+            return True
+        elif num % 2 == 0:
+            return self.isUgly(num/2)
+        elif num % 3 == 0:
+            return self.isUgly(num/3)
+        elif num % 5 == 0:
+            return self.isUgly(num/5)
+        else:
+            return False
