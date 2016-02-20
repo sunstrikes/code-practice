d855b60be634941574bc07e614bde010e44a1b9c
Mickey Sun <sunstrikes@163.com>
2016-02-20 22:04:49 +0800

    Hash

---
+++ LeetCode/Algorithms/332. Reconstruct Itinerary.py
@@ -0,0 +1,14 @@
+import collections
+class Solution(object):
+    def findItinerary(self, tickets):
+        targets = collections.defaultdict(list)
+        for a, b in sorted(tickets)[::-1]:
+            targets[a].append(b)
+        route = []
+        def visit(airport):
+            while targets[airport]:
+                visit(targets[airport].pop())
+            route.append(airport)
+        visit('JFK')
+        return route[::-1]
+
